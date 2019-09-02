b719403e68c0b0b32c1f8ff1d4d0b130d8cc6a67
Troy <troy@meishakeji.com>
2019-09-02 11:22:46 +0800

    init

---
+++ client/page/Index/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <div>hello world</div>
+      </div>
+    );
+  }
+}
+
+export default Index;
---
+++ server/router/index.js
@@ -0,0 +1,7 @@
+const router = require('koa-router')();
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'hello world';
+});
+
+module.exports = router;
